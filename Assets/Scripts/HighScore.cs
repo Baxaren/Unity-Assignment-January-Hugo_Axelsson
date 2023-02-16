61a7468f5990492f7e0bb795512615ba083e10af
Hugo Axelsson <hugobax@yahoo.com>
2023-02-16 13:34:10 +0100

    Fixed Score
    Fixed the Score issue, should now show at start and remember between startups.

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -1,38 +1,44 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class HighScore : MonoBehaviour
 {
-    public Text score;
-    public Text highScore;
+    public TMP_Text score;
+    public TMP_Text highScore;
     public int NumberScore;
 
-    void start()
+    void Start()
     {
-        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        int v = PlayerPrefs.GetInt("HighScore", 0);
+        highScore.text = v.ToString("000");
+        Debug.Log($"Start score {v}");
 
     }
+
 
     public void ScoreGained ()
     {
         NumberScore = NumberScore + 1;  // add 1 to score
-        score.text = NumberScore.ToString();
+        score.text = NumberScore.ToString("000");
 
         if( NumberScore > PlayerPrefs.GetInt("HighScore", 0))  // if score is more than high score, record high score
         {
             PlayerPrefs.SetInt("HighScore", NumberScore);
-            highScore.text = NumberScore.ToString();
+            highScore.text = NumberScore.ToString("000");
 
         }
+
+
 
     }
 
     public void ResetScore()  // reset highscore as well as current score
     {
         PlayerPrefs.DeleteKey("HighScore");
-        highScore.text = "0";
-        score.text = "0";
+        highScore.text = "000";
+        score.text = "000";
         NumberScore = 0;
 
     }
